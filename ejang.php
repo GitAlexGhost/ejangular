efd461a1eb45c83f90e9a14133b928f9923dd42d
AC.VB <alex.acvb@ac.cl>
2020-08-05 13:01:32 -0400

    Comit body

---
+++ ejang.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div ng-app='' ng-controller='miControlador'>
+{{nombre+' '+apellidos}}
+</div>
+<script>
+function miControlador($scope){
+$scope.nombre='Alex';
+$scope.apellidos='CF';
+}
+</script>
+<script src='//ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js'></script>
+</body>
+</html>
+
+<html>
+<title> Js controlador pacientes</title>
+<head>
+<script src=""></script>	
+</head>
+</html>
+
+
+
